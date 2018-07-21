000c8bbfec106c0ce8dd854139a907c058f00b85
edoh <edoh.dev@gmail.com>
2018-07-21 20:52:20 +0200

    add markers

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -4,6 +4,19 @@ import ReactDOM from 'react-dom'
 
 export default class MapContainer extends Component {
 
+  state = {
+    locations: [
+      { name: "Ensimag", location: {lat: 45.1931492, lng: 5.7674826999999596} },
+      { name: "Atos", location: {lat: 45.1539228, lng: 5.7207387999999355} },
+      { name: "Museum", location: {lat: 45.1949173, lng: 5.732278299999962} },
+      { name: "Stadium", location: {lat: 45.1874353, lng: 5.740127799999982} },
+      { name: "Mall", location: {lat: 45.158158, lng: 5.731906999999978} }
+    ],
+    query: '',
+    markers: [],
+    infowindow: new this.props.google.maps.InfoWindow()
+  }
+
   componentDidMount() {
     this.loadMap()
 
@@ -24,8 +37,45 @@ export default class MapContainer extends Component {
       })
 
       this.map = new maps.Map(node, mapConfig)
+      this.addMarkers()
     }
 
+  }
+
+  addMarkers = () => {
+    const {google} = this.props
+    let {infowindow} = this.state
+    const bounds = new google.maps.LatLngBounds();
+
+    this.state.locations.forEach( (location, ind) => {
+      const marker = new google.maps.Marker({
+        position: {lat: location.location.lat, lng: location.location.lng},
+        map: this.map,
+        title: location.name
+      });
+
+      marker.addListener('click', () => {
+        this.populateInfoWindow(marker, infowindow)
+      })
+      this.setState((state) => ({
+        markers: [...state.markers, marker]
+      }))
+      bounds.extend(marker.position)
+    })
+    this.map.fitBounds(bounds)
+  }
+
+  populateInfoWindow = (marker, infowindow) => {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker !== marker) {
+      infowindow.marker = marker;
+      infowindow.setContent(`<h3>title</h3><h4>user likes it</h4>`);
+      infowindow.open(this.map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
   }
 
   render() {
